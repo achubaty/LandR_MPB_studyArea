8eba2ca66d42e1f662e0ea6077d54b06653dc4aa
Alex Chubaty <achubaty@for-cast.ca>
2021-05-26 20:00:19 -0600

    move study area creation to mpbutils::mpbStudyArea()

--- MPB_SK_studyArea.R
+++ MPB_SK_studyArea.R
@@ -11,7 +11,8 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = deparse(list("README.md", "MPB_SK_studyArea.Rmd")), ## same file
-  reqdPkgs = list("ggplot2", "ggspatial", "raster", "sf"),
+  reqdPkgs = list("ggplot2", "ggspatial", "raster", "sf",
+                  "PredictiveEcology/mpbutils"),
   parameters = rbind(
     defineParameter(".plotInitialTime", "numeric", NA, NA, NA,
                     "Describes the simulation time at which the first plot event should occur."),
@@ -32,9 +33,7 @@ defineModule(sim, list(
   ),
   outputObjects = bindrows(
     createsOutput(objectName = "studyArea", objectClass = "SpatialPolygonsDataFrame",
-                  desc = "buffered study area for simulation"),
-    createsOutput(objectName = "studyAreaFit", objectClass = "SpatialPolygonsDataFrame",
-                  desc = "unbuffered study area used for fitting spread data"),
+                  desc = "buffered study area for simulation and fitting"),
     createsOutput(objectName = "studyAreaReporting", objectClass = "SpatialPolygonsDataFrame",
                   desc = "unbuffered study area for reporting/post-processing"),
   )
@@ -95,40 +94,34 @@ Init <- function(sim) {
 
   absk <- provinces[provinces$NAME_1 %in% c("Alberta", "Saskatchewan"), ]
 
-  ecoregions <- prepInputs(url = "http://sis.agr.gc.ca/cansis/nsdb/ecostrat/region/ecoregion_shp.zip",
-                           targetFile = "ecoregions.shp", alsoExtract = "similar",
-                           fun = "sf::st_read",
-                           cacheRepo = cachePath(sim),
-                           destinationPath = inputPath(sim)) %>%
-    st_transform(., sim$targetCRS) ## keep as sf for plotting
-
   ## study area ecoregions:
   ##   Wabasca Lowlands (112)
   ##   Mid-Boreal Uplands (122, 124, 126)
   ##   Western Alberta Uplands (120)
-  studyAreaReporting <- ecoregions[ecoregions$REGION_ID %in% c(112, 122, 124, 126), ]
-  studyAreaFit <- ecoregions[ecoregions$REGION_ID %in% c(120), ]
+  studyAreaReporting <- mpbStudyArea(ecoregions = c(112, 120, 122, 124, 126),
+                                     targetCRS = sim$targetCRS,
+                                     cPath = cachePath(sim),
+                                     dPath = mod$dPath) %>%
+    st_intersection(., absk) %>%
+    st_union(.)
   studyArea <- st_buffer(studyAreaReporting, 10000) ## 10 km buffer
-  studyAreasJoined <- ecoregions[ecoregions$REGION_ID %in% c(112, 122, 124, 126, 120), ] %>%
-    st_intersection(., absk)
 
   cols <- c("darkgreen", "forestgreen", "darkred")
   mod$gg_studyAreas <- ggplot(absk) +
     geom_sf(fill = "white", colour = "black", alpha = 0.5) +
-    geom_sf(data = studyAreasJoined, mapping = aes(fill = REGION_NAM, colour = REGION_NAM), alpha = 0.5) +
+    geom_sf(data = studyArea, mapping = aes(fill = REGION_NAM, colour = REGION_NAM), alpha = 0.5) +
     theme_bw() +
     annotation_north_arrow(location = "bl", which_north = "true",
                            pad_x = unit(0.25, "in"), pad_y = unit(0.25, "in"),
                            style = north_arrow_fancy_orienteering) +
     xlab("Longitude") + ylab("Latitude") +
-    ggtitle("MPB study areas") +
+    ggtitle("MPB study area") +
     scale_colour_manual(values = cols) +
     scale_fill_manual(values = cols)
 
   ## convert to spdf for use with other modules
-  sim$studyAreaReporting <- as_Spatial(studyAreaReporting)
-  sim$studyAreaFit <- as_Spatial(studyAreaFit)
   sim$studyArea <- as_Spatial(studyArea)
+  sim$studyAreaReporting <- as_Spatial(studyAreaReporting)
 
   # ! ----- STOP EDITING ----- ! #
 
@@ -137,8 +130,8 @@ Init <- function(sim) {
 
 .inputObjects <- function(sim) {
   #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
-  dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
-  message(currentModule(sim), ": using dataPath '", dPath, "'.")
+  mod$dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
+  message(currentModule(sim), ": using dataPath '", mod$dPath, "'.")
 
   # ! ----- EDIT BELOW ----- ! #
   if (!suppliedElsewhere("targetCRS")) {
